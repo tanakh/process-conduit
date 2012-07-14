a5d846837275608f5e3d51bb50b1ff538e7b68e0
Simon Hengel <sol@typeful.net>
2012-07-14 17:57:32 +0200

    Use Hspec's `shouldBe` instead of HUnit's @?=

--- test.hs
+++ test.hs
@@ -7,30 +7,29 @@ import qualified Data.ByteString.Lazy.Char8 as L
 import Data.Conduit
 import qualified Data.Conduit.Binary as CB
 import Test.Hspec
-import Test.HUnit
 
 main :: IO ()
 main = hspec $ do
   describe "process conduit" $ do
     it "get process's output" $ do
       r <- runResourceT $ sourceCmd "echo abc def" $$ CB.take (10^9)
-      L.words r @?= ["abc", "def"]
+      L.words r `shouldBe` ["abc", "def"]
 
     it "act as conduit" $ do
       r <- runResourceT $
         sourceProcess (proc "echo" ["zxc\nasd\nqwe"])
         $$ conduitCmd "sort"
         =$ CB.take (10^9)
-      L.words r @?= ["asd", "qwe", "zxc"]
+      L.words r `shouldBe` ["asd", "qwe", "zxc"]
 
   describe "quasiquoter" $ do
     it "get process's output" $ do
       r <- [cmd|echo abc def|]
-      L.words r @?= ["abc", "def"]
+      L.words r `shouldBe` ["abc", "def"]
 
     it "act as conduit" $ do
       r <- runResourceT $
         sourceProcess (proc "echo" ["zxc\nasd\nqwe"])
         $$ [ccmd|sort|]
         =$ CB.take (10^9)
-      L.words r @?= ["asd", "qwe", "zxc"]
+      L.words r `shouldBe` ["asd", "qwe", "zxc"]
